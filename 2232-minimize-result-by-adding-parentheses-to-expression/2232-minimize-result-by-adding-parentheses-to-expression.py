e0f3b2b8136852cf7004f76f157b35d5ec5fc23d
Sumanth Reddy <sumanth368@gmail.com>
2022-10-20 12:53:54 -0700

    Time: 54 ms (65.10%), Space: 14.1 MB (29.32%) - LeetHub

--- 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.py
+++ 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.py
@@ -1,12 +1,22 @@
-class Solution:
-    def minimizeResult(self, expression: str) -> str:
-        plus_index, n, ans = expression.find('+'), len(expression), [float(inf),expression] 
-        def evaluate(exps: str):
-            return eval(exps.replace('(','*(').replace(')', ')*').lstrip('*').rstrip('*'))
-        for l in range(plus_index):
-            for r in range(plus_index+1, n):
-                exps = f'{expression[:l]}({expression[l:plus_index]}+{expression[plus_index+1:r+1]}){expression[r+1:n]}'
-                res = evaluate(exps)
-                if ans[0] > res:
-                    ans[0], ans[1] = res, exps
-        return ans[1]
+class Solution(object):
+    def minimizeResult(self, e: str) -> str:
+        res = '('+e+')'
+        e = e.split('+')
+        mn = int(e[0])+int(e[1])
+        
+        m, n = len(e[0]), len(e[1])
+        
+        for i in range(-1,m-1):
+            a = int(e[0][:i+1]) if i>=0 else 1
+            b = int(e[0][i+1:])
+            
+            for j in range(n):
+                c = int(e[1][:j+1])
+                d = int(e[1][j+1:]) if j+1 < n else 1
+                calc = a*(b+c)*d
+                print(a,b,c,d,calc)
+                if mn > calc:
+                    res = (str(a) if i>=0 else '')+'('+str(b)+'+'+str(c)+')'+(str(d) if j+1 < n else '')
+                    mn = calc
+        return res
+        
