3ec08a149d3010a6095db371c30168080bfff67e
Sumanth Reddy <sumanth368@gmail.com>
2022-10-20 12:52:35 -0700

    Time: 68 ms (24.61%), Space: 13.8 MB (79.58%) - LeetHub

--- 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.py
+++ 2232-minimize-result-by-adding-parentheses-to-expression/2232-minimize-result-by-adding-parentheses-to-expression.py
@@ -1,17 +1,12 @@
 class Solution:
     def minimizeResult(self, expression: str) -> str:
-        left, right = expression.split("+")
-        smallest_val = float("inf")
-        smallest_expression = ""
-        for i in range(len(left)):
-            for j in range(1, len(right)+1):
-                a = 1 if i == 0 else int(left[:i])
-                b = int(left[i:])
-                c = int(right[:j])
-                d = 1 if j == len(right) else int(right[j:])
-                expression_val = a*(b+c)*d
-                expression = left[:i]+'('+left[i:]+'+'+right[:j]+')'+right[j:]
-                if expression_val < smallest_val:
-                    smallest_val = expression_val
-                    smallest_expression = expression
-        return smallest_expression
+        plus_index, n, ans = expression.find('+'), len(expression), [float(inf),expression] 
+        def evaluate(exps: str):
+            return eval(exps.replace('(','*(').replace(')', ')*').lstrip('*').rstrip('*'))
+        for l in range(plus_index):
+            for r in range(plus_index+1, n):
+                exps = f'{expression[:l]}({expression[l:plus_index]}+{expression[plus_index+1:r+1]}){expression[r+1:n]}'
+                res = evaluate(exps)
+                if ans[0] > res:
+                    ans[0], ans[1] = res, exps
+        return ans[1]
