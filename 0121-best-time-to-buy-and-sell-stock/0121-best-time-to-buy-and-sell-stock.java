1148b61de1518dd4a1a1122986aaeeb7b274a6e4
Sumanth Reddy <sumanth368@gmail.com>
2022-11-05 16:45:41 -0700

    Time: 30 ms (5.11%), Space: 76.4 MB (77.65%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,37 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE;
-        int op = 0;
-        int pist = 0;
+        if (prices == null || prices.length < 2) return 0;
+        return maxProfitHelper(prices, 0, prices.length - 1);
+    }
+    
+    public int maxProfitHelper (int[] prices, int l, int r) {
+        if (r == l) return 0;
+        if (r == l + 1) {
+            if (prices[l] < prices[r]) return prices[r] - prices[l];
+            else return 0;
+        }
         
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
+        int mid = l + (r - l) / 2;
+        int leftMax = maxProfitHelper(prices, l, mid);
+        int rightMax = maxProfitHelper(prices, mid + 1, r);
+        
+        // min is in [l, mid], max is in [mid+1, r];
+        
+        // find min
+        int minVal = prices[l];
+        for (int i = l + 1; i <= mid; i++) {
+            if (prices[i] < minVal) {
+                minVal = prices[i];
             }
         }
-        return op;
+        
+        int maxVal = prices[mid+1];
+        for (int i = mid + 2; i <= r; i++) {
+            if (prices[i] > maxVal) {
+                maxVal = prices[i];
+            }
+        }
+        int crossDiff = maxVal - minVal;
+        return Math.max(leftMax, Math.max(rightMax, crossDiff));
     }
 }
